0aed95fa90705947711e8725acbaec6b37af16cb
Jesus Trejo <jesustrejo10@gmail.com>
2017-08-24 13:18:39 -0400

    Prubas con Scroll view

--- LibrariesFlyers/app/src/main/java/com/test/trejo/jesus/librariesflyers/LonelyEffect/LonelyEffectActivity.java
+++ LibrariesFlyers/app/src/main/java/com/test/trejo/jesus/librariesflyers/LonelyEffect/LonelyEffectActivity.java
@@ -2,14 +2,41 @@ package com.test.trejo.jesus.librariesflyers.LonelyEffect;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
+import com.lsjwzh.widget.recyclerviewpager.RecyclerViewPager;
+import com.test.trejo.jesus.librariesflyers.HorizontalRecycler.HorizontalAdapter;
+import com.test.trejo.jesus.librariesflyers.HorizontalRecycler.RecyclerObject;
 import com.test.trejo.jesus.librariesflyers.R;
 
+import java.util.ArrayList;
+
 public class LonelyEffectActivity extends AppCompatActivity {
+
+    private ArrayList<RecyclerObject> mDataSet;
+    private VerticalLonelyAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lonely_effect);
+
+        RecyclerObject test = new RecyclerObject();
+        mDataSet = new ArrayList<>();
+        mDataSet.add(test);
+        mDataSet.add(test);
+        mDataSet.add(test);
+        mDataSet.add(test);
+        mDataSet.add(test);
+        mDataSet.add(test);
+        mDataSet.add(test);
+        mAdapter = new VerticalLonelyAdapter(mDataSet);
+
+        RecyclerView mRecyclerView = (RecyclerView) findViewById(R.id.list);
+        // setLayoutManager like normal RecyclerView, you do not need to change any thing.
+        LinearLayoutManager layout = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(layout);
+        mRecyclerView.setAdapter(mAdapter);
     }
 }
--- LibrariesFlyers/app/src/main/java/com/test/trejo/jesus/librariesflyers/LonelyEffect/VerticalLonelyAdapter.java
+++ LibrariesFlyers/app/src/main/java/com/test/trejo/jesus/librariesflyers/LonelyEffect/VerticalLonelyAdapter.java
@@ -1,8 +1,48 @@
 package com.test.trejo.jesus.librariesflyers.LonelyEffect;
 
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.test.trejo.jesus.librariesflyers.HorizontalRecycler.HorizontalViewHolder;
+import com.test.trejo.jesus.librariesflyers.HorizontalRecycler.RecyclerObject;
+import com.test.trejo.jesus.librariesflyers.R;
+
+import java.util.ArrayList;
+
 /**
  * Created by jesus on 24/08/17.
  */
 
-public class VerticalLonelyAdapter {
+public class VerticalLonelyAdapter  extends RecyclerView.Adapter<HorizontalViewHolder> {
+
+    private ArrayList<RecyclerObject> mDataSet;
+
+    public VerticalLonelyAdapter(ArrayList<RecyclerObject> mDataset) {
+        this.mDataSet = mDataset;
+    }
+
+    @Override
+    public HorizontalViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        // create a new view
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.horizontal_item, parent, false);
+        // set the view's size, margins, paddings and layout parameters
+        HorizontalViewHolder vh = new HorizontalViewHolder(v);
+        return vh;
+    }
+
+
+    @Override
+    public void onBindViewHolder(HorizontalViewHolder holder, int position) {
+
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return mDataSet.size();
+    }
+
 }
