f8e7b26ae9c98a6a6e4d1ee87dcd0baa29fe33b1
Jesus Trejo <jesustrejo10@gmail.com>
2017-08-25 12:42:43 -0400

    test

--- LibrariesFlyers/app/src/main/java/com/test/trejo/jesus/librariesflyers/LonelyEffect/LonelyEffectActivity.java
+++ LibrariesFlyers/app/src/main/java/com/test/trejo/jesus/librariesflyers/LonelyEffect/LonelyEffectActivity.java
@@ -5,7 +5,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AbsListView;
+import android.widget.ScrollView;
 
 import com.lsjwzh.widget.recyclerviewpager.RecyclerViewPager;
 import com.sothree.slidinguppanel.SlidingUpPanelLayout;
@@ -19,12 +23,23 @@ public class LonelyEffectActivity extends AppCompatActivity {
 
     private ArrayList<RecyclerObject> mDataSet;
     private VerticalLonelyAdapter mAdapter;
+    private ScrollView mScrollView;
     SlidingUpPanelLayout panel;
-
+    LinearLayoutManager mLayoutManager;
+    RecyclerView mRecyclerView;
+    int visibleItemCount ;
+    int totalItemCount ;
+    int firstVisibleItemIndex ;
+    boolean loading = false;
+    int previousTotal = 0;
+    int status = 1;
+    //0 mean collapsed
+    //1 mean hidden
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lonely_effect);
+
 
         RecyclerObject test = new RecyclerObject();
         mDataSet = new ArrayList<>();
@@ -38,27 +53,17 @@ public class LonelyEffectActivity extends AppCompatActivity {
         mAdapter = new VerticalLonelyAdapter(mDataSet);
 
         panel = (SlidingUpPanelLayout) findViewById(R.id.sliding_layout);
-        panel.setScrollContainer(true);
-        /*
-        final int someWaitInterval = 20;
-        Runnable r = new Runnable() {
 
-            @Override
-            public void run() {
-                try {
-                    panel.setPanelHeight(50);
-                } catch (Exception e2) {
-                    new Handler().postDelayed(this, someWaitInterval);
-                }
-            }};
-        new Handler().postDelayed(r, someWaitInterval);
-        */
+
+        final int someWaitInterval = 20;
+
+
 //                                                                                                                          panel.
 
-        RecyclerView mRecyclerView = (RecyclerView) findViewById(R.id.list);
+        mRecyclerView = (RecyclerView) findViewById(R.id.list);
         // setLayoutManager like normal RecyclerView, you do not need to change any thing.
-        LinearLayoutManager layout = new LinearLayoutManager(this);
-        mRecyclerView.setLayoutManager(layout);
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
         /*
         if (layout.findFirstCompletelyVisibleItemPosition() == 0) {
             panel.setPanelHeight(50);
@@ -67,5 +72,73 @@ public class LonelyEffectActivity extends AppCompatActivity {
         }
         */
         mRecyclerView.setAdapter(mAdapter);
+        mRecyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+
+
+                //WORK FOR BOTTOM
+                int visibleItemCount = mLayoutManager.getChildCount();
+                int totalItemCount = mLayoutManager.getItemCount();
+                int pastVisibleItems = mLayoutManager.findFirstVisibleItemPosition();
+
+                if(pastVisibleItems == 0){
+                    if (status != 0) {
+                        Runnable r = new Runnable() {
+
+                            @Override
+                            public void run() {
+                                try {
+                                    status = 0;
+                                    panel.setPanelState(SlidingUpPanelLayout.PanelState.COLLAPSED);                                } catch (Exception e2) {
+                                    new Handler().postDelayed(this, someWaitInterval);
+                                }
+                            }};
+                        new Handler().postDelayed(r, someWaitInterval);
+
+                    }
+                }else{
+                    Runnable r = new Runnable() {
+
+                        @Override
+                        public void run() {
+                            try {
+                                status = 0;
+                                panel.setPanelState(SlidingUpPanelLayout.PanelState.HIDDEN);                                } catch (Exception e2) {
+                                new Handler().postDelayed(this, someWaitInterval);
+                            }
+                        }};
+                    new Handler().postDelayed(r, someWaitInterval);
+                    //if (pastVisibleItems + visibleItemCount >= totalItemCount) {
+                    //}
+                }
+
+/*
+                visibleItemCount = mRecyclerView.getChildCount();
+                totalItemCount = mLayoutManager.getItemCount();
+                firstVisibleItemIndex = mLayoutManager.findFirstVisibleItemPosition();
+                //synchronizew loading state when item count changes
+                if (loading) {
+                    if (totalItemCount > previousTotal) {
+                        loading = false;
+                        previousTotal = totalItemCount;
+                    }
+                }
+                if (!loading)
+                    if ((totalItemCount - visibleItemCount) <= firstVisibleItemIndex) {
+                        // Loading NOT in progress and end of list has been reached
+                        // also triggered if not enough items to fill the screen
+                        // if you start loading
+                        loading = true;
+                    } else if (firstVisibleItemIndex == 0){
+                        // top of list reached
+                        // if you start loading
+                        loading = true;
+                    }
+*/
+            }
+        });
     }
+
+
 }
