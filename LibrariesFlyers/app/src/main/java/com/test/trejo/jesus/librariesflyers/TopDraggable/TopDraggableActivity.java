e2378269e21d30f295d37480af5c51474483b7a4
Jesus Trejo <jesustrejo10@gmail.com>
2017-08-29 10:52:32 -0400

    Sliding Up avanzado

--- LibrariesFlyers/app/src/main/java/com/test/trejo/jesus/librariesflyers/TopDraggable/TopDraggableActivity.java
+++ LibrariesFlyers/app/src/main/java/com/test/trejo/jesus/librariesflyers/TopDraggable/TopDraggableActivity.java
@@ -1,16 +1,71 @@
 package com.test.trejo.jesus.librariesflyers.TopDraggable;
 
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.LinearLayout;
 
+import com.sothree.slidinguppanel.SlidingUpPanelLayout;
 import com.test.trejo.jesus.librariesflyers.R;
 
 public class TopDraggableActivity extends AppCompatActivity {
 
+    SlidingUpPanelLayout mPanel;
+    LinearLayout mMainLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_top_draggable);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Hoteles Disponibles");
+
+        mPanel= (SlidingUpPanelLayout) findViewById(R.id.sliding_layout);
+        mMainLayout = (LinearLayout) findViewById(R.id.main_layout);
+        mMainLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mPanel.setPanelState(SlidingUpPanelLayout.PanelState.COLLAPSED);
+            }
+        });
+        mPanel.setFadeOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mPanel.setPanelState(SlidingUpPanelLayout.PanelState.COLLAPSED);
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.draggable_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.action_filter:
+                final Handler handler = new Handler();
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        //Do something after 1000ms
+                        mPanel.setPanelState(SlidingUpPanelLayout.PanelState.EXPANDED);
+                    }
+                }, 100);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
 
