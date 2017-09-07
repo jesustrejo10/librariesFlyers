ffd08caf0e9cb62e06630d43fae5c6ea2e5f0ee7
Nelson Ramirez <nelsonalfo@gmail.com>
2017-09-07 18:54:36 -0400

    buscando la forma de que se muestre el snapshot y sin tener que mostrar de primera mano el layout que contiene el fragmento del mapa

--- LibrariesFlyers/app/src/main/java/com/test/trejo/jesus/librariesflyers/ToolbarEffect/ToolbarEffectActivity.java
+++ LibrariesFlyers/app/src/main/java/com/test/trejo/jesus/librariesflyers/ToolbarEffect/ToolbarEffectActivity.java
@@ -1,19 +1,26 @@
 package com.test.trejo.jesus.librariesflyers.ToolbarEffect;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.alexvasilkov.gestures.views.GestureImageView;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
@@ -24,6 +31,9 @@ import com.test.trejo.jesus.librariesflyers.R;
  */
 
 public class ToolbarEffectActivity extends AppCompatActivity implements OnMapReadyCallback {
+    private ImageView mapPreview;
+    private GestureImageView mapPreviewFull;
+    private MapView mapView;
 
 
     @Override
@@ -33,6 +43,28 @@ public class ToolbarEffectActivity extends AppCompatActivity implements OnMapRea
 
         final LinearLayout toolbarContent = findViewById(R.id.toolbar_layout);
         final TextView toolbarTitle = findViewById(R.id.toolbar_title);
+        final FrameLayout mapFullLayout = findViewById(R.id.maps_full_layout);
+        final Button closeMapButton = findViewById(R.id.close_map_button);
+        final CoordinatorLayout coordinator = findViewById(R.id.coordinator);
+
+        mapPreview = findViewById(R.id.map_image);
+        mapPreviewFull = findViewById(R.id.map_preview_full);
+
+        mapPreview.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mapFullLayout.setVisibility(View.VISIBLE);
+                coordinator.setVisibility(View.GONE);
+            }
+        });
+
+        closeMapButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mapFullLayout.setVisibility(View.GONE);
+                coordinator.setVisibility(View.VISIBLE);
+            }
+        });
 
         final Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -67,7 +99,7 @@ public class ToolbarEffectActivity extends AppCompatActivity implements OnMapRea
         });
     }
 
-    @Override
+      @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_tab_activity_action_bar, menu);
         return true;
@@ -84,8 +116,22 @@ public class ToolbarEffectActivity extends AppCompatActivity implements OnMapRea
     }
 
     @Override
-    public void onMapReady(GoogleMap googleMap) {
+    public void onMapReady(final GoogleMap googleMap) {
         LatLng sydney = new LatLng(-33.867, 151.206);
+
+        googleMap.setOnMapLoadedCallback(new GoogleMap.OnMapLoadedCallback() {
+            @Override
+            public void onMapLoaded() {
+                // Make a snapshot when map's done loading
+                googleMap.snapshot(new GoogleMap.SnapshotReadyCallback() {
+                    @Override
+                    public void onSnapshotReady(Bitmap bitmap) {
+                        mapPreview.setImageBitmap(bitmap);
+                    }
+                });
+            }
+        });
+
         googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 13));
 
         googleMap.addMarker(new MarkerOptions()
