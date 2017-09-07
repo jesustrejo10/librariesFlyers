03b3624e2d75ca04f89428371212e2904ae8f499
Nelson Ramirez <nelsonalfo@gmail.com>
2017-09-07 19:49:40 -0400

    refactorizando codigo para hacerlo mas limpio

--- LibrariesFlyers/app/src/main/java/com/test/trejo/jesus/librariesflyers/ToolbarEffect/ToolbarEffectActivity.java
+++ LibrariesFlyers/app/src/main/java/com/test/trejo/jesus/librariesflyers/ToolbarEffect/ToolbarEffectActivity.java
@@ -2,14 +2,13 @@ package com.test.trejo.jesus.librariesflyers.ToolbarEffect;
 
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.GestureDetector;
-import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.MotionEvent;
@@ -44,13 +43,18 @@ public class ToolbarEffectActivity extends AppCompatActivity implements OnMapRea
     private GestureImageView mapPreviewFull;
     private Toolbar toolbar;
     private ViewsTransitionAnimator animator;
+    private FrameLayout mapFullLayout;
+    private Button closeMapButton;
+    private CoordinatorLayout coordinator;
+    private LinearLayout toolbarContent;
+    private TextView toolbarTitle;
+    private AppBarLayout appBarLayout;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_toolbar_alternative);
-
 
         toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -60,6 +64,15 @@ public class ToolbarEffectActivity extends AppCompatActivity implements OnMapRea
             supportActionBar.setDisplayHomeAsUpEnabled(true);
             supportActionBar.setDisplayShowTitleEnabled(false);
         }
+
+        toolbarContent = findViewById(R.id.toolbar_layout);
+        toolbarTitle = findViewById(R.id.toolbar_title);
+        appBarLayout = findViewById(R.id.app_bar_layout);
+        mapFullLayout = findViewById(R.id.maps_full_layout);
+        closeMapButton = findViewById(R.id.close_map_button);
+        coordinator = findViewById(R.id.coordinator);
+        mapPreview = findViewById(R.id.map_image);
+        mapPreviewFull = findViewById(R.id.map_preview_full);
 
         final MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
 
@@ -71,81 +84,20 @@ public class ToolbarEffectActivity extends AppCompatActivity implements OnMapRea
     }
 
     private void initViews() {
-        final FrameLayout mapFullLayout = findViewById(R.id.maps_full_layout);
-        final Button closeMapButton = findViewById(R.id.close_map_button);
-        final CoordinatorLayout coordinator = findViewById(R.id.coordinator);
-
-        mapPreview = findViewById(R.id.map_image);
-        mapPreviewFull = findViewById(R.id.map_preview_full);
-
         mapPreviewFull.getController().getSettings()
                 .setPanEnabled(false)
                 .setZoomEnabled(false)
                 .setDoubleTapEnabled(false)
-                .setRotationEnabled(false)
-                .setRestrictRotation(false)
                 .setOverscrollDistance(this, 0f, 0f)
                 .setOverzoomFactor(1f)
                 .setExitEnabled(false)
-                .setFillViewport(true)
                 .setFitMethod(Settings.Fit.VERTICAL)
-                .setGravity(Gravity.CENTER)
                 .setAnimationsDuration(Settings.ANIMATIONS_DURATION);
 
-        mapPreviewFull.getPositionAnimator().addPositionUpdateListener(new ViewPositionAnimator.PositionUpdateListener() {
-            @Override
-            public void onPositionUpdate(float position, boolean isLeaving) {
-                if (position == 1) {
-                    mapFullLayout.setVisibility(View.VISIBLE);
-                    mapPreviewFull.setVisibility(View.GONE);
-                    coordinator.setVisibility(View.GONE);
-                } else if (isLeaving) {
-                    mapPreviewFull.setVisibility(View.VISIBLE);
-                    coordinator.setVisibility(View.VISIBLE);
-                    mapFullLayout.setVisibility(View.GONE);
-                }
-            }
-        });
-
-        mapPreview.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                mapFullLayout.setVisibility(View.VISIBLE);
-                coordinator.setVisibility(View.GONE);
-            }
-        });
-
-        closeMapButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                mapPreviewFull.getPositionAnimator().exit(true);
-            }
-        });
-
-        GesturesDetectorListener gesturesDetectorListener = new GesturesDetectorListener() {
-            @Override
-            public boolean onDoubleTap(MotionEvent motionEvent) {
-                Log.d("NELSON", "onDoubleTap");
-                openFullImage();
-                return true;
-            }
-
-            @Override
-            public boolean onScale(ScaleGestureDetector detector) {
-                double defaultScaleFactor = 1;
-                Log.d("NELSON", "onScale - detector.getScaleFactor() = " + detector.getScaleFactor());
-                if (detector.getScaleFactor() > defaultScaleFactor) {
-                    openFullImage();
-                }
-                return true;
-            }
-        };
-
+        final GesturesDetectorListener gesturesDetectorListener = getNewGesturesDetectorListener();
+        final ScaleGestureDetector scaleGestureDetector = new ScaleGestureDetector(this, gesturesDetectorListener);
         final GestureDetector gestureDetector = new GestureDetector(this, gesturesDetectorListener);
         gestureDetector.setOnDoubleTapListener(gesturesDetectorListener);
-
-        final ScaleGestureDetector scaleGestureDetector = new ScaleGestureDetector(this, gesturesDetectorListener);
-
 
         mapPreview.setOnTouchListener(new View.OnTouchListener() {
             @Override
@@ -157,20 +109,57 @@ public class ToolbarEffectActivity extends AppCompatActivity implements OnMapRea
         });
 
         animator = GestureTransitions.from(mapPreview).into(mapPreviewFull);
-
         animator.addPositionUpdateListener(new ViewPositionAnimator.PositionUpdateListener() {
             @Override
             public void onPositionUpdate(float position, boolean isLeaving) {
                 if (position == 0f && isLeaving)
                     mapPreviewFull.setVisibility(View.INVISIBLE);
-                else
+                else if (position == 1) {
+                    mapFullLayout.setVisibility(View.VISIBLE);
+                    coordinator.setVisibility(View.GONE);
+                } else if (isLeaving) {
+                    mapFullLayout.setVisibility(View.GONE);
+                    coordinator.setVisibility(View.VISIBLE);
+                } else {
                     mapPreviewFull.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+
+        closeMapButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                animator.exit(true);
             }
         });
     }
 
-    private void openFullImage() {
-        // Setting image drawable from 'from' view to 'to' to prevent flickering
+    @NonNull
+    private GesturesDetectorListener getNewGesturesDetectorListener() {
+        return new GesturesDetectorListener() {
+            @Override
+            public boolean onDoubleTap(MotionEvent motionEvent) {
+                openFullMap();
+                return true;
+            }
+
+            @Override
+            public boolean onScale(ScaleGestureDetector detector) {
+                openFullMapWhenZoomIn(detector);
+                return true;
+            }
+
+            private void openFullMapWhenZoomIn(ScaleGestureDetector detector) {
+                final double defaultScaleFactor = 1;
+                if (detector.getScaleFactor() > defaultScaleFactor) {
+                    openFullMap();
+                }
+            }
+        };
+    }
+
+    private void openFullMap() {
+        // Setting image drawable from mapPreviewView to mapPreviewFullView to prevent flickering
         if (mapPreviewFull.getDrawable() == null) {
             mapPreviewFull.setImageDrawable(mapPreview.getDrawable());
         }
@@ -182,10 +171,6 @@ public class ToolbarEffectActivity extends AppCompatActivity implements OnMapRea
     }
 
     private void configureToolbarBehavior() {
-        final LinearLayout toolbarContent = findViewById(R.id.toolbar_layout);
-        final TextView toolbarTitle = findViewById(R.id.toolbar_title);
-        final AppBarLayout appBarLayout = findViewById(R.id.app_bar_layout);
-
         appBarLayout.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
             @Override
             public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
